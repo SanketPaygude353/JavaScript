b87b8042c4d61ec01c62bc55e32759f54028ceb5
sanket <sanketpaygude195@gmail.com>
2025-10-17 11:59:32 +0530

    learn typeConversion

---
+++ 01_Basics/03_typeConversion.js
@@ -0,0 +1,41 @@
+// let score = 11;
+
+// console.log(typeof score);
+
+// let a= "11"
+// let b=Number(a)
+
+// console.log(typeof b)
+
+
+// let abc="11abc"
+// let lmn=Number(abc)
+
+// console.log(typeof lmn);
+// console.log(lmn);    //================>Here's the catch it gives converted number as NAN not a number
+
+// console.log(typeof NaN)    // =>>Number
+// console.log(typeof null)   // =>>Object
+// console.log(typeof undefined)  // =>>Undefined
+
+///================================in boolean case
+
+// let isLogged=""
+
+// let booleanIsLogged=Boolean(isLogged)
+
+// console.log(booleanIsLogged);
+
+
+// let value =3
+// let negValue=-value
+// console.log(negValue);  //==> o/p -3
+
+// console.log("1" + 2)  //o/p ==>12
+// console.log(1 + "2")  //o/p ==>12
+// console.log("1" + 2 + 2) //o/p ==>122
+// console.log(1 + 2 + "2")//o/p ==>32
+
+//console.log(+true); //o/p ==>1
+//console.log(+"")  //o/p ==> 0
+
