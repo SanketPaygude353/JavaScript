6fbb9145ff9caedf0505cba8835e6c0515ae0e93
sanket <sanketpaygude195@gmail.com>
2025-10-17 17:54:38 +0530

    learned about dates

---
+++ 01_Basics/07_dates_inJS.js
@@ -0,0 +1,11 @@
+let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof(myDate));
+
+let createDate=new Date(2023,0,25)
+console.log(createDate.toDateString())
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
