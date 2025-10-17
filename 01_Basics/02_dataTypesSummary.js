d86e6de80c88ef509281e911c84d9eb2ac23ba9f
sanket <sanketpaygude195@gmail.com>
2025-10-17 12:42:34 +0530

    Revision Of DataTyeps

---
+++ 01_Basics/02_dataTypesSummary.js
@@ -0,0 +1,36 @@
+//Primitive 
+/* 
+    7 types : String, Number, Boolean, null, undefined, Symbol,
+    BigInt
+*/
+
+//Reference(Non Primitive)
+/*
+    Array, Objects, Functions
+*/
+//==================================================================================
+//JS is dynamically typed language
+
+// const id=Symbol(123);
+// const anotherId=Symbol(123)
+
+// console.log(id==anotherId); //O/p==> false
+
+//Reference(Non Primitive)
+/*
+    Array, Objects, Functions
+*/
+
+// const heros=["ironMan","thor","captainAmerica"]
+
+// let myObj={
+//     name:"sanket",
+//     age:22,
+// }
+
+// const myFunction=function(){
+//     console.log("Hello JS");
+    
+// }
+
+// console.log(myFunction);
