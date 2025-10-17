ef7b2592fa10e240a119485626ecbbd725568e45
sanket <sanketpaygude195@gmail.com>
2025-10-17 16:46:23 +0530

    learned nums and maths

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+// const score = 400;
+
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(4));
+
+// const otherNumber=123.0215448;
+
+// console.log(otherNumber.toPrecision(3));
+
+// console.log(Math)
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6));
+
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+
+// console.log(Math.min(4,6,3));
+
+// console.log(Math.max(4,6,3));
+
+let randomNum = Math.floor(Math.random() * 11); 
+console.log(randomNum);
