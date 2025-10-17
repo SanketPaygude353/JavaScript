aba33be888e0e49a970306f638f113cdd6518ab4
sanket <sanketpaygude195@gmail.com>
2025-10-17 10:44:12 +0530

    learn datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountEmail="sanketpaygude195@gmail.com"
+var accountPassword="21212121"
+let accountId=123
+accountCity="Pune"
+let accountState;
+// accountEmail="sanketpaygude19@gmail.com"
+// accountPassword="2121212"
+// accountCity="Pun"
+
+/*
+Prefer not to use var 
+because of block and functional scope
+*/
+
+console.table([accountEmail,accountPassword,accountCity,accountId,accountState]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"  //treat all JS code as newer version
+
+//alert(3+3);  we are using nodejs not browser so alert can only run on browser
+
+//number=> 12
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>undefined
+//symbol=> unique
+//Object
+
+console.log(typeof null)
