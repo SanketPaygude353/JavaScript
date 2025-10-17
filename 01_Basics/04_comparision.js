01b4960021f3b7ffdc75f4db763adccb9728cc71
sanket <sanketpaygude195@gmail.com>
2025-10-17 12:23:57 +0530

    learned Comparisions in JS

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1)  
+// console.log("02" > 1)
+
+//here out put of both line is true becuase it converts it to number
+
+
+// console.log(null>0)  //o/p===>false
+// console.log(null<0)  //o/p===>false
+// console.log(null==0)  //o/p===>false
+// console.log(null>=0)  //o/p===>true
+// console.log(null<=0)  //o/p===>true
+
+// console.log(2=="2") //o/p==>true   checks for value only
+// console.log(2==="2") //o/p==>false  checks for the datatypes also
+
+
+
