5bcfb1e00298d3bf5220d04799f08a6112465088
sanket <sanketpaygude195@gmail.com>
2025-10-18 21:59:23 +0530

    leaned object literals

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,11 @@
+//object literals
+
+const JsUser = {
+    name:"sanket",
+    age:22,
+    email:"sanketpaygude195@gmail.com",
+    location:"pune",
+}
+
+console.log(JsUser.name);
+console.log(JsUser["email"]);
