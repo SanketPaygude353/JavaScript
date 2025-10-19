90969f023b420f9bfc6145e7f67ca13bc886082a
sanket <sanketpaygude195@gmail.com>
2025-10-19 22:42:26 +0530

    Learned function 4 types

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,32 @@
+
+//1.simple function without parameter without returning anything
+// function sayMyName(){
+//     console.log("Sanket");
+// }
+
+// sayMyName()
+
+//2.with parameter without returning
+// function add(number1,number2,number3){
+//     console.log(number1+number2+number3);
+// }
+
+// add(2,3,"4")
+
+//3.without parameter with returning
+
+// function forReturn(){
+//     return 4+5
+// }
+
+// const ans=forReturn()
+// console.log(ans);
+
+
+//4.with parameter with return
+function addition(num1,num2){
+    return num1+num2
+}
+
+const ans=addition(50,100);
+console.log(ans);
