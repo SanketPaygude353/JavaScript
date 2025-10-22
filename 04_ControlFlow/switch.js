7610e4dd42f99b736af7012d332386384fadcfda
sanket <sanketpaygude195@gmail.com>
2025-10-22 23:05:12 +0530

    Learned switch cases

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,56 @@
+const month=3
+
+switch(month)
+{
+    case 1:
+        console.log("jan");
+        break;
+        case 1:
+    case 2:
+        console.log("feb");
+        break;
+        case 1:
+    case 3:
+        console.log("Mar");
+        break;
+        case 1:
+    case 4:
+        console.log("Apr");
+        break;
+        case 1:
+    case 5:
+        console.log("May");
+        break;
+        case 1:
+    case 6:
+        console.log("jun");
+        break;
+        case 1:
+    case 7:
+        console.log("jul");
+        break;
+        case 1:
+    case 8:
+        console.log("Aug");
+        break;
+        case 1:
+    case 9:
+        console.log("Sept");
+        break;
+        case 1:
+    case 10:
+        console.log("Oct");
+        break;
+        case 1:
+    case 11:
+        console.log("Nov");
+        break;
+        case 1:
+    case 12:
+        console.log("Dec");
+        break;
+    default:
+        console.log("Invalid Choice");
+        
+        
+}
