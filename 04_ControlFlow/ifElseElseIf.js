0e78edc1a82cccccdd72728465fcb787b352e655
sanket <sanketpaygude195@gmail.com>
2025-10-22 22:59:02 +0530

    If-Else, If Else ladder learned

---
+++ 04_ControlFlow/ifElseElseIf.js
@@ -0,0 +1,36 @@
+
+// if(true)
+// {
+//     console.log("Hello")
+// }
+
+// if(false){
+//     console.log("Js");
+// }
+
+
+const marks=80
+
+if(marks>75)
+{
+    console.log("Grade A");
+}
+else if(marks>60)
+{
+    console.log("Grade B");
+}
+
+else if(marks>45)
+{
+    console.log("Grade C");
+}
+
+else if(marks>35)
+{
+    console.log("Pass");
+}
+else{
+    console.log("Fail");
+    
+}
+
