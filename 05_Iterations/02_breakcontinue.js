7f226d93df9eb6fa390cc8f585763f2eaf53666b
sanket <sanketpaygude195@gmail.com>
2025-10-23 22:02:58 +0530

    learned break and continue

---
+++ 05_Iterations/02_breakcontinue.js
@@ -0,0 +1,24 @@
+
+
+// for (let i = 0; i < 10; i++) {
+    
+//     if(i==5)
+//     {
+//         console.log(i);
+//         break
+//         console.log(i)
+//     }
+    
+// }
+for (let i = 0; i < 10; i++) {
+    
+    if(i==5)
+    {
+        //console.log("let",i);
+        continue
+        //console.log(i)
+    }
+    console.log(i);
+    
+    
+}
