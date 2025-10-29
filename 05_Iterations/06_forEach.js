62f667b30ecb7b317e91707697da6c862729de4c
sanket <sanketpaygude195@gmail.com>
2025-10-29 19:37:31 +0530

    learned forEach

---
+++ 05_Iterations/06_forEach.js
@@ -0,0 +1,33 @@
+const coding=["js","ruby","py","cpp"]
+
+coding.forEach((item)=>{
+  //  console.log(item);
+    
+})
+
+
+coding.forEach((item,index,arr)=>{
+    //console.log(item, index,arr);
+    
+})
+
+
+let myCoding=[
+    {
+        language: "javaScript",
+        langFile: "js"
+    },
+    {
+        language:"Python",
+        langFile:"py"
+    },
+    {
+        language:"Java",
+        langFile:"java"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.language);
+    
+})
