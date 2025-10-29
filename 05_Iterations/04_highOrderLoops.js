0d9c7a729b47b9ad024d9c1b5e14585ae24978b2
sanket <sanketpaygude195@gmail.com>
2025-10-29 19:14:35 +0530

    learned about higherOrder loops'

---
+++ 05_Iterations/04_highOrderLoops.js
@@ -0,0 +1,27 @@
+
+let arr=[1,2,3,4,5,6,7]
+
+for(let value of arr)
+{
+   // console.log(value);
+    
+}
+
+let str="hello world"
+for(let val of str){
+  //  console.log(val);
+}
+
+const map=new Map()
+map.set("IN","India")
+map.set("USA","United States of America")
+map.set("Uk","United Kingdom")
+
+//console.log(map);
+
+for(let [key,value] of map)
+{
+   // console.log(key, ':-', value);
+    
+}
+
