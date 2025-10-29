b44f20b4e2865f969098dfc659c0ff11728018d0
sanket <sanketpaygude195@gmail.com>
2025-10-29 19:25:12 +0530

    loops for obj

---
+++ 05_Iterations/05_loopsForObj.js
@@ -0,0 +1,19 @@
+const myObj={
+    js:"JavaScript",
+    py:"Python",
+    cpp:"C++",
+    rb:"Ruby",
+}
+
+for (const key in myObj) {
+   // console.log(myObj[key]);
+}
+
+
+//it can be used for array also
+const myArr=["js","java","cpp","py"]
+
+for (const key in myArr) {
+    console.log(myArr[key]);
+    
+}
